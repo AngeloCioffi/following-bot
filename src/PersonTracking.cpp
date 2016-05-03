7a875853ed65df3c5d52695e1adcf06822c461b0
zaralouis <zaralouis13@gmail.com>
2016-05-03 17:22:45 -0500

    Update PersonTracking.cpp

--- src/PersonTracking.cpp
+++ src/PersonTracking.cpp
@@ -19,6 +19,54 @@
  
 using namespace std;
 using namespace cv;
+
+class ImageConverter
+{
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
+  image_transport::Publisher image_pub_;
+  
+  
+public:
+	//constructor, creating an ImageConev
+  ImageConverter()
+    : it_(nh_)
+  {
+    // Subscrive to input video feed and publish output video feed
+    image_sub_ = it_.subscribe("/nav_kinect/rgb/image_color", 1, 
+      &ImageConverter::imageCb, this);
+    image_pub_ = it_.advertise("/image_converter/output_video", 1);
+
+    cv::namedWindow(OPENCV_WINDOW);
+  }
+
+  ~ImageConverter()
+  {
+    cv::destroyWindow(OPENCV_WINDOW);
+  }
+
+  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  {
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+    
+ 
+    cv::Mat outImg;
+	outImg = cv_ptr->image.clone(); 
+	
+
+
+}
+
  
 int main (int argc, const char * argv[])
 {
